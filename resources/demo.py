5cc74327094ff2ce2c5fe45662b0bab95882ef15
Brian Mo <brianmo@BrianMacPro.local>
2018-08-23 14:44:44 -0700

    add for demo

--- resources/demo.py
+++ resources/demo.py
@@ -3,5 +3,5 @@ from flask_restful import Resource
 class Demo(Resource):
 
     def get(self):
-        return {'message': "Hi good afternoon, it's 10:35 now"},200
+        return {'message': "Hi good afternoon, it's 14:4 now"},200
    
